482b95a583491b746b336b1f7b30a42437c93f86
Michał Policht <michal85so@gmail.com>
2016-08-17 19:58:40 +0200

    - default constructor in entities

--- src/main/java/pl/policht/smws/entity/Address.java
+++ src/main/java/pl/policht/smws/entity/Address.java
@@ -7,14 +7,19 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="address")
 public class Address {
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
--- src/main/java/pl/policht/smws/entity/Person.java
+++ src/main/java/pl/policht/smws/entity/Person.java
@@ -7,11 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="person")
 public class Person {
