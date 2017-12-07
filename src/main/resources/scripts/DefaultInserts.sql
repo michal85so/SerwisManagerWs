e4f32a15605bf78704f33e28ac35fcb00a039032
Michał Policht <michal85so@gmail.com>
2017-12-07 16:49:25 +0100

    sql script fix

--- src/main/resources/scripts/DefaultInserts.sql
+++ src/main/resources/scripts/DefaultInserts.sql
@@ -9,8 +9,12 @@ insert into dictionary_of_service_status(name) values('new');
 insert into dictionary_of_service_status(name) values('in progress');
 insert into dictionary_of_service_status(name) values('done');
 insert into dictionary_of_service_status(name) values('received');
+
 insert into dictionary_of_city(name) values('default');
+
 insert into dictionary_of_state(name) values('default');
+
 insert into address(city_id,post_code,street,state_id) values(1,'default','default',1);
-INSERT INTO `sm`.`person` (`login`,`password`,`first_name`,`last_name`,`status_id`,`address_id`,`phone_number`,`email`,`nip`,`regon`)
+
+INSERT INTO person (login,password,first_name,last_name,status_id,address_id,phone_number,email,nip,regon)
 					VALUES('default','default','default','default',1,1,'default','default','default','default');
